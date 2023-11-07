c34161128a22ca942dcba142ece137976659408b
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-11-07 17:45:47 +0900

    Feat: mutation을 이용한 포스트 삭제 기능 구현

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 // eslint-disable-next-line
 async function fetchComments(postId) {
@@ -30,12 +30,26 @@ export function PostDetail({ post }) {
 		queryKey: ['comments', post.id],
 		queryFn: () => fetchComments(post.id),
 	});
+
+	const deleteMutation = useMutation(() => deletePost(post.id));
+	// const deleteMutation = useMutation((postId) => deletePost(postId));
 	if (isLoading) return <h3>loading...</h3>;
 	if (isError) return <h3>Error! {error.toString()}</h3>;
 	return (
 		<>
 			<h3 style={{ color: 'blue' }}>{post.title}</h3>
-			<button>Delete</button> <button>Update title</button>
+			<button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+			{deleteMutation.isError && (
+				<p style={{ color: 'red' }}>Error Deleting the post</p>
+			)}
+			{deleteMutation.isLoading && (
+				<p style={{ color: 'purple' }}>Deleting the post</p>
+			)}
+			{deleteMutation.isSuccess && (
+				<p style={{ color: 'green' }}>The post has been deleted</p>
+			)}
+
+			<button>Update title</button>
 			<p>{post.body}</p>
 			<h4>Comments</h4>
 			{data.map((comment) => (
