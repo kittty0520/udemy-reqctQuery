628594b551c72af688aa759063aee81493fed861
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-11-07 16:00:23 +0900

    Feat: 포스트 및 댓글 호출 기능 구현

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,41 +1,48 @@
+import { useQuery } from '@tanstack/react-query';
+
+// eslint-disable-next-line
 async function fetchComments(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
-  );
-  return response.json();
+	const response = await fetch(
+		`https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+	);
+	return response.json();
 }
-
+// eslint-disable-next-line
 async function deletePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "DELETE" }
-  );
-  return response.json();
+	const response = await fetch(
+		`https://jsonplaceholder.typicode.com/postId/${postId}`,
+		{ method: 'DELETE' }
+	);
+	return response.json();
 }
-
+// eslint-disable-next-line
 async function updatePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
-  );
-  return response.json();
+	const response = await fetch(
+		`https://jsonplaceholder.typicode.com/postId/${postId}`,
+		{ method: 'PATCH', data: { title: 'REACT QUERY FOREVER!!!!' } }
+	);
+	return response.json();
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
-
-  return (
-    <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
-      <p>{post.body}</p>
-      <h4>Comments</h4>
-      {data.map((comment) => (
-        <li key={comment.id}>
-          {comment.email}: {comment.body}
-        </li>
-      ))}
-    </>
-  );
+	// replace with useQuery
+	const { data, isError, isLoading, error } = useQuery({
+		queryKey: ['comments', post.id],
+		queryFn: () => fetchComments(post.id),
+	});
+	if (isLoading) return <h3>loading...</h3>;
+	if (isError) return <h3>Error! {error.toString()}</h3>;
+	return (
+		<>
+			<h3 style={{ color: 'blue' }}>{post.title}</h3>
+			<button>Delete</button> <button>Update title</button>
+			<p>{post.body}</p>
+			<h4>Comments</h4>
+			{data.map((comment) => (
+				<li key={comment.id}>
+					{comment.email}: {comment.body}
+				</li>
+			))}
+		</>
+	);
 }
