165db3616d7b6ab977222c19e9052607ed5987c3
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-11-15 16:22:04 +0900

    Feat: 전역 에러 핸들러 등록 및 useTreatment훅에서 에러 핸들러 제거

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -1,21 +1,27 @@
-// import { createStandaloneToast } from '@chakra-ui/react';
-// import { theme } from '../theme';
-
+import { createStandaloneToast } from '@chakra-ui/react';
 import { QueryClient } from '@tanstack/react-query';
 
-// const toast = createStandaloneToast({ theme });
+import { theme } from '../theme';
 
-// function queryErrorHandler(error: unknown): void {
-//   // error is type unknown because in js, anything can be an error (e.g. throw(5))
-//   const title =
-//     error instanceof Error ? error.message : 'error connecting to server';
+const toast = createStandaloneToast({ theme });
 
-//   // prevent duplicate toasts
-//   toast.closeAll();
-//   toast({ title, status: 'error', variant: 'subtle', isClosable: true });
-// }
+function queryErrorHandler(error: unknown): void {
+  // error is type unknown because in js, anything can be an error (e.g. throw(5))
+  const title =
+    error instanceof Error ? error.message : 'error connecting to server';
+
+  // prevent duplicate toasts
+  toast.closeAll();
+  toast({ title, status: 'error', variant: 'subtle', isClosable: true });
+}
 
 // to satisfy typescript until this file has uncommented contents
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      onError: queryErrorHandler,
+    },
+  },
+});
 export { queryClient };
