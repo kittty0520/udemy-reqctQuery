df6724f55ac572043f0f389d6903eebd4c8141ef
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-11-16 15:00:46 +0900

    Feat: staff 정보를 prefetching함

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 
 import type { Staff } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
@@ -24,9 +24,15 @@ export function useStaff(): UseStaff {
 
   const fallback = [];
 
+  const selectFn = useCallback(
+    (data: Staff[]) => filterByTreatment(data, filter),
+    [filter],
+  );
+
   const { data: staff = fallback } = useQuery({
     queryKey: [queryKeys.staff],
     queryFn: getStaff,
+    select: filter === 'all' ? undefined : selectFn,
   });
 
   return { staff, filter, setFilter };
