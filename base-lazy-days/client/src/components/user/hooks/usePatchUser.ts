04f5cf7cf65e3e9a85eae37f62d5b72dcbb32c5a
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-12-09 02:00:59 +0900

    Feat: mutation 응답으로 사용자 캐시 업데이트

--- base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
+++ base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
@@ -1,3 +1,5 @@
+import { UseMutateFunction, useMutation } from '@tanstack/react-query';
+import { useCustomToast } from 'components/app/hooks/useCustomToast';
 import jsonpatch from 'fast-json-patch';
 
 import type { User } from '../../../../../shared/types';
@@ -5,33 +7,45 @@ import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
 import { useUser } from './useUser';
 
 // for when we need a server function
-// async function patchUserOnServer(
-//   newData: User | null,
-//   originalData: User | null,
-//  ): Promise<User | null> {
-//   if (!newData || !originalData) return null;
-//   // create a patch for the difference between newData and originalData
-//   const patch = jsonpatch.compare(originalData, newData);
+async function patchUserOnServer(
+  newData: User | null,
+  originalData: User | null,
+): Promise<User | null> {
+  if (!newData || !originalData) return null;
+  // create a patch for the difference between newData and originalData
+  const patch = jsonpatch.compare(originalData, newData);
 
-//   // send patched data to the server
-//   const { data } = await axiosInstance.patch(
-//     `/user/${originalData.id}`,
-//     { patch },
-//     {
-//       headers: getJWTHeader(originalData),
-//     },
-//   );
-//   return data.user;
-//  }
+  // send patched data to the server
+  const { data } = await axiosInstance.patch(
+    `/user/${originalData.id}`,
+    { patch },
+    {
+      headers: getJWTHeader(originalData),
+    },
+  );
+  return data.user;
+}
 
-// TODO: update type to UseMutateFunction type
-export function usePatchUser(): (newData: User | null) => void {
+export function usePatchUser(): UseMutateFunction<
+  User,
+  unknown,
+  User,
+  unknown
+> {
   const { user, updateUser } = useUser();
-
-  // TODO: replace with mutate function
-  const patchUser = (newData: User | null) => {
-    // nothing to see here
-  };
+  const toast = useCustomToast();
+  const { mutate: patchUser } = useMutation({
+    mutationFn: (newUserData: User) => patchUserOnServer(newUserData, user),
+    onSuccess: (newUserData: User | null) => {
+      if (user) {
+        updateUser(newUserData);
+        toast({
+          title: 'The user information has updated',
+          status: 'success',
+        });
+      }
+    },
+  });
 
   return patchUser;
 }
